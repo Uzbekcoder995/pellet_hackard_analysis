a6ba4ffd0dc525e4041992b588c4037df250ae9d
Saidakramov <akbarsaidakramov@gmail.com>
2024-05-21 09:38:46 -0700

    adding queries, new retirement info csv file

--- schema.sql
+++ schema.sql
@@ -15,9 +15,10 @@
 	 hire_date DATE NOT NULL,
 	 PRIMARY KEY (emp_no)
  );
+ --DROP TABLE dept_manager;
  
  CREATE TABLE dept_manager (
- 	dept_no VARCHAR(4) NOT NULL,
+ 	dept_no VARCHAR NOT NULL,
 	 emp_no int NOT NULL,
 	 from_date DATE NOT NULL,
 	 to_date DATE NOT NULL,
@@ -35,13 +36,13 @@
 	 PRIMARY KEY (emp_no)
  );
  
+  --DROP TABLE titles;
  CREATE TABLE titles (
  	emp_no INT NOT NULL,
 	 title VARCHAR NOT NULL,
 	 from_date DATE NOT NULL,
 	 to_date DATE NOT NULL,
-	 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	 PRIMARY KEY (emp_no)
+	 FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
  );
  
  CREATE TABLE dept_emp (
@@ -53,7 +54,3 @@
 	 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	 PRIMARY KEY (emp_no, dept_no)
  );
- 
- SELECT
-  * 
- FROM departments;
